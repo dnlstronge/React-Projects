3ac2968a1373619a204bb46e621cbb1b44ce390d
Dan Stronge <dnlstronge@gmail.com>
2022-10-27 15:51:43 +0100

    Imported files from X

--- myfirstreactapp/src/App.js
+++ myfirstreactapp/src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          text here
+         
         </a>
       </header>
     </div>
