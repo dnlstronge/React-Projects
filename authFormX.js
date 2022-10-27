351af187c80e6c3c598a8e453176e7e35c2e124b
Dan Stronge <dnlstronge@gmail.com>
2022-10-27 09:46:41 +0100

    add displays contact info on successful password check

--- authFormX.js
+++ authFormX.js
@@ -1,43 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      password: 'swordfish',
+      password: "swordfish",
       authorized: false
     };
     this.authorize = this.authorize.bind(this);
   }
 
   authorize(e) {
-    const password = e.target.querySelector(
-      'input[type="password"]').value;
+    const password = e.target.querySelector('input[type="password"]').value;
     const auth = password == this.state.password;
     this.setState({
-      authorized: auth
+      authorized: auth,
     });
   }
 
   render() {
+    const login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input type="password" placeholder="Password" />
+        <input type="submit" />
+      </form>
+    )
+    const contactInfo = (
+      <ul>
+        <li>client@example.com</li>
+        <li>555.555.5555</li>
+      </ul>
+    );
+
     return (
       <div id="authorization">
-        <h1>Contact</h1>
-        <ul>
-          <li>
-            client@example.com
-          </li>
-          <li>
-            555.555.5555
-          </li>
-        </ul>
+        <h1>{this.state.authorized ? "Contact" : "Enter the Password"}</h1>
+        {this.state.authorized ? contactInfo : login}
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <Contact />, 
-  document.getElementById('app')
-);
+ReactDOM.render(<Contact />, document.getElementById("app"));
