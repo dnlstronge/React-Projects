5e66bb903be431fc22b6bf2253292f37593776c7
Dan Stronge <dnlstronge@gmail.com>
2022-10-28 09:29:31 +0100

    creat: add get time create: add state change set interval fix: clock now updates every second

--- BrokenClock/BrokenClock.js
+++ BrokenClock/BrokenClock.js
@@ -1,16 +1,22 @@
 
 //FIX THIS 
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Clock extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {date: new Date()}
-    }
+    super(props);
+    this.state = { date: new Date() };
+  }
   render() {
-    return <div></div>}
+    return <div>{this.state.date.toLocaleTimeString()}</div>;
+  }
+  componentDidMount() {
+   const oneSecond = 1000;
+   setInterval(() => {
+     this.setState({date: new Date()})
+   }, oneSecond);
+  }
 }
 
 ReactDOM.render(<Clock />, document.getElementById('app'));
