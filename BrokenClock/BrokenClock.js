13639ad010f1a37da18c25961638e247f6872036
Dan Stronge <dnlstronge@gmail.com>
2022-10-28 09:21:38 +0100

    create: add constructor + add render set state

--- BrokenClock/BrokenClock.js
+++ BrokenClock/BrokenClock.js
@@ -1,12 +1,16 @@
 
 //FIX THIS 
 
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Clock extends React.Component {
-  // Add your methods in here.
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+    }
+  render() {
+    return <div></div>}
 }
 
 ReactDOM.render(<Clock />, document.getElementById('app'));
