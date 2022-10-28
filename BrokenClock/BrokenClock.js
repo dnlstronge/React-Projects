332e155da942b6c30f4d393555f2e97317ad5fd7
Dan Stronge <dnlstronge@gmail.com>
2022-10-28 10:04:59 +0100

    create: add precise mode + toggle fix: make precise mode update more frequent

--- BrokenClock/BrokenClock.js
+++ BrokenClock/BrokenClock.js
@@ -9,7 +9,13 @@ export class Clock extends React.Component {
     this.state = { date: new Date() };
   }
   render() {
-    return <div>{this.state.date.toLocaleTimeString()}</div>;
+    return (
+      <div>
+        {this.props.isPrecise
+          ? this.state.date.toISOString()
+          : this.state.date.toLocaleTimeString()}
+      </div>
+    );
   }
   componentDidMount() {
     const oneSecond = 1000;
@@ -17,6 +23,15 @@ export class Clock extends React.Component {
       this.setState({ date: new Date() });
     }, oneSecond);
   }
-componentWillUnmount() {clearInterval(this.intervalID)
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props.isPrecise === prevProps.isPrecise) {return;} 
+    clearInterval(this.intervalID); 
+    let delay 
+    this.intervalID = setInterval(() => {
+      this.setState({ date: new Date()})
+    }, delay )
   }
 }
