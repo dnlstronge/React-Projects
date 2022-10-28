71aee3204897136df31c0330376d19db13e462a2
Dan Stronge <dnlstronge@gmail.com>
2022-10-28 09:15:27 +0100

    creat: init repo

---
+++ BrokenClock/BrokenClock.js
@@ -0,0 +1,12 @@
+
+//FIX THIS 
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Clock extends React.Component {
+  // Add your methods in here.
+}
+
+ReactDOM.render(<Clock />, document.getElementById('app'));
