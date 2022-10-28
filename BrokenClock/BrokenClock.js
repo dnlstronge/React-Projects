fe1cb569520e8e7d4ebd11d3e30006f7dd360347
Dan Stronge <dnlstronge@gmail.com>
2022-10-28 09:47:13 +0100

    fix: cleanup after set interval

--- BrokenClock/BrokenClock.js
+++ BrokenClock/BrokenClock.js
@@ -1,11 +1,9 @@
 
 //FIX THIS 
 
-
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
+export class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = { date: new Date() };
@@ -14,16 +12,11 @@ class Clock extends React.Component {
     return <div>{this.state.date.toLocaleTimeString()}</div>;
   }
   componentDidMount() {
-   const oneSecond = 1000;
-   setInterval(() => {
-     this.setState({date: new Date()})
-   }, oneSecond);
+    const oneSecond = 1000;
+    this.intervalID = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, oneSecond);
+  }
+componentWillUnmount() {clearInterval(this.intervalID)
   }
 }
-
-ReactDOM.render(<Clock />, document.getElementById('app'));
-
-/*problem: how will the clock stop??
- the timer could keep on ticking, trying to update
- a component which has gone running uneccessary JS
-*/
