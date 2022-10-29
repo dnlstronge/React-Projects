7d150d1b8fa366c7c71936e204aa01b8c5229459
Dan Stronge <dnlstronge@gmail.com>
2022-10-29 14:20:13 +0100

    // add helper to determine winner

--- myfirstreactapp/src/index.js
+++ myfirstreactapp/src/index.js
@@ -26,7 +26,8 @@ import './index.css';
   }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]}
+      return <Square 
+      value={this.state.squares[i]}
       onClick={() => this.handleClick(i)} />;
     }
   
@@ -76,3 +77,24 @@ import './index.css';
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(<Game />);
+  // helper
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
