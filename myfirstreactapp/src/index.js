9cb6f7c65c26dce22d6a074c5fcc44b9d912e738
Dan Stronge <dnlstronge@gmail.com>
2022-10-29 12:32:26 +0100

    add event listener add statechange to x on click

--- myfirstreactapp/src/index.js
+++ myfirstreactapp/src/index.js
@@ -1,8 +1,18 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
 class Square extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {value: null};
+    }
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" 
+        onClick={() => {this.setState({value: 'x'})}} >
+          {this.state.value}
         </button>
       );
     }
@@ -10,7 +20,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
