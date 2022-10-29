4eb08d0acab7d717a4a69e0959b3599fef595e82
Dan Stronge <dnlstronge@gmail.com>
2022-10-29 10:45:08 +0100

    create: refactor, add more colors use array to store colors

--- ColorPicker/colorPicker.js
+++ ColorPicker/colorPicker.js
@@ -1,17 +1,22 @@
-// import the default export and the named export `useState` from the 'react' library
-import React, { useState } from "react";
+import React, { useState } from 'react';
+
+const colorNames = ['Aquamarine', 'BlueViolet', 'Chartreuse', 'CornflowerBlue', 'Thistle', 'SpringGreen', 'SaddleBrown', 'PapayaWhip', 'MistyRose'];
+
 export default function ColorPicker() {
-  // call useState and assign its return values to `color` and `setColor`
   const [color, setColor] = useState();
-  const divStyle = { backgroundColor: color };
+
+ const divStyle = {backgroundColor: color};
 
   return (
     <div style={divStyle}>
-      <p>The color is {color}</p>
-      <button onClick={() => setColor("Aquamarine")}>Aquamarine</button>
-      <button onClick={() => setColor("BlueViolet")}>BlueViolet</button>
-      <button onClick={() => setColor("Chartreuse")}>Chartreuse</button>
-      <button onClick={() => setColor("CornflowerBlue")}>CornflowerBlue</button>
+      <p>Selected color: {color}</p>
+      {colorNames.map((colorName)=>(
+        <button 
+          onClick={() => setColor(colorName)} 
+          key={colorName}>
+       	     {colorName}
+      	</button>
+      ))}
     </div>
   );
 }
