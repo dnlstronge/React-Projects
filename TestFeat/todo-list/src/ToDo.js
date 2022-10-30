dcd2ebe825195ef40f5c258158dd1234510fc37f
Dan Stronge <dnlstronge@gmail.com>
2022-10-30 14:20:38 +0000

    fix: rename header to Header add ToDoForm add handlers for user input

--- TestFeat/todo-list/src/ToDo.js
+++ TestFeat/todo-list/src/ToDo.js
@@ -1,13 +1,18 @@
 // row of to-do
-
 import React from 'react';
- 
-const ToDo = ({todo}) => {
-   return (
-       <div>
-           {todo.task}
-       </div>
-   );
+
+const ToDo = ({todo, handleToggle}) => {
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        handleToggle(e.currentTarget.id)
+    }
+
+    return (
+        <div id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} onClick={handleClick} className={todo.complete ? "todo strike" : "todo"}>
+            {todo.task}
+        </div>
+    );
 };
- 
+
 export default ToDo;
