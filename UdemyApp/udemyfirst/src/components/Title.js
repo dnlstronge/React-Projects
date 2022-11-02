c9edfc7162c8ee1648cc38ddeef9804e569c9f2b
Dan Stronge <dnlstronge@gmail.com>
2022-11-02 13:02:03 +0000

    add props and destructure

--- UdemyApp/udemyfirst/src/components/Title.js
+++ UdemyApp/udemyfirst/src/components/Title.js
@@ -1,7 +1,11 @@
-export default function Title(props) {
+
+//add in the props from app.js(Title), note I have used
+//destructuring here {}, avoids the need for props.'x'
+
+export default function Title({titleprop, newprop}) {
     return (
         <div>
-            <h1 className="title">{props.titleprop} <br /> {props.newprop}</h1>
+            <h1 className="title">{titleprop} <br /> {newprop}</h1>
             <br />
             <h2 className="subtitle">Latest Events</h2>
         </div> 
