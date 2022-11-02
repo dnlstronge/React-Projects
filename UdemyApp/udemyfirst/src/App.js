d5aa0f8117a25ce711c90ccb85c66adfe6dac3fe
Dan Stronge <dnlstronge@gmail.com>
2022-11-02 08:38:17 +0000

    create: add new array of objects test handler works

--- UdemyApp/udemyfirst/src/App.js
+++ UdemyApp/udemyfirst/src/App.js
@@ -6,21 +6,40 @@ import {useState} from 'react';
 
 function App() {
 
-  let [name, setName] = useState("Steve")
+  // change to an array of objects
+  // give each an id
 
-let handleClick = () => {
-  setName("kevin")
-  alert(`${name} is the man`)
-  console.log(name)
+  let [events, setevent] = useState([
+    {title: "Dr", surname: "Foster", id: 1},
+    {title: "Lord", surname: "Finchester", id: 2},
+    {title: "Ms", surname: "Hayberbrook", id: 3}
+  ])
+
+  // check handler works: should output 1, 2, 3 in console (clear)
+let handleClick = (id) => {
+
+ console.log(id)
+
 };
 
   return (
+  
     <div className="App">
-    <h1>His name was {name}</h1>
-    <button onClick={handleClick}>Change name</button>
+
+    {events.map((element, index) => (
+    <div key={element.id}>
+     
+    <h2> {index} His name was {element.surname}</h2>
+    <button onClick={() => handleClick(element.id)}>Delete element </button>
     </div>
+    ))}
+    
+    </div>
+    
   );
 }
+
+
 // important :)
 
 export default App;
