c6b0f7552107c3d655b809ed34b91ca6ae89a15f
Dan Stronge <dnlstronge@gmail.com>
2022-11-03 13:17:04 +0000

    create: gave new state, find inline method for returning state change from button

--- UdemyApp/udemyfirst/src/App.js
+++ UdemyApp/udemyfirst/src/App.js
@@ -4,6 +4,7 @@ import React, {useState} from 'react';
 import Title from './components/Title';
 import Modal from './components/Modal';
 import EventList from './components/EventList';
+import NewEventForm from './components/NewEventForm';
 
 
 
@@ -69,12 +70,7 @@ const red = "Lots of exciting happenings a happening!"
     {showEvents && (<EventList handleClick={handleClick} events={events}/>) }
 
       {showModal && <Modal handleClose={handleClose} isSalesModal={true} >
-      <h2>Terms and conditions
-              <p>Discount is subject to availability, excess consumption of fun may lead
-              to repeat bouts of hysteria. Big Dave Promotions does not admit liabilty for any torn muscles
-              induced by way of overexposure to laughter.</p>
-          </h2>
-        
+      <NewEventForm />
       </Modal> }
       <button onClick={() => setShowModal(true)}>Event Form</button>
 
--- UdemyApp/udemyfirst/src/components/NewEventForm.js
+++ UdemyApp/udemyfirst/src/components/NewEventForm.js
@@ -1,17 +1,31 @@
 import "./NewEventForm.css"
+import {useState} from 'react'
 
 import React from 'react'
 
+
+
 export default function NewEventForm() {
+
+    const [title, setTitle] = useState('')
+    const [date, setDate] = usedState('')
+
+   /* const handleChange = (e) => {
+        setTitle(e.target.value)
+        
+    }
+    */
+
+
   return (
     <form clasName="new-event-form">
         <label>
         <span>Event Title</span>
-            <input type="text" />
+            <input onChange={handleChange} type="text" />
         </label>
         <label>
         <span>Event Date</span>
-            <input type="date" />
+            <input onChange={handleChange} type="date" />
         </label>
         <button>Submit</button>
     </form>
