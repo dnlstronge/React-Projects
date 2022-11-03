620f56df13d4737720650977949c4913a742012a
Dan Stronge <dnlstronge@gmail.com>
2022-11-03 13:20:37 +0000

    create: add inline anon func inline for set title and set date

--- UdemyApp/udemyfirst/src/components/NewEventForm.js
+++ UdemyApp/udemyfirst/src/components/NewEventForm.js
@@ -8,7 +8,7 @@ import React from 'react'
 export default function NewEventForm() {
 
     const [title, setTitle] = useState('')
-    const [date, setDate] = usedState('')
+    const [date, setDate] = useState('')
 
    /* const handleChange = (e) => {
         setTitle(e.target.value)
@@ -21,11 +21,11 @@ export default function NewEventForm() {
     <form clasName="new-event-form">
         <label>
         <span>Event Title</span>
-            <input onChange={handleChange} type="text" />
+            <input onChange={(e) => setTitle(e.target.value) } type="text" />
         </label>
         <label>
         <span>Event Date</span>
-            <input onChange={handleChange} type="date" />
+            <input onChange={(e) => setDate(e.target.value)} type="date" />
         </label>
         <button>Submit</button>
     </form>
