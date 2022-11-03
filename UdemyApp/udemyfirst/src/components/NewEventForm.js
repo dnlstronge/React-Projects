952af7b3792c773e1db5d2a3d9a76f5f50f0ed15
Dan Stronge <dnlstronge@gmail.com>
2022-11-03 12:52:40 +0000

    create: add styles for new event form.

--- UdemyApp/udemyfirst/src/components/NewEventForm.js
+++ UdemyApp/udemyfirst/src/components/NewEventForm.js
@@ -4,6 +4,16 @@ import React from 'react'
 
 export default function NewEventForm() {
   return (
-    <form clasName="new-event-form"></form>
+    <form clasName="new-event-form">
+        <label>
+        <span>Event Title</span>
+            <input type="text" />
+        </label>
+        <label>
+        <span>Event Date</span>
+            <input type="date" />
+        </label>
+        <button>Submit</button>
+    </form>
   )
 }
