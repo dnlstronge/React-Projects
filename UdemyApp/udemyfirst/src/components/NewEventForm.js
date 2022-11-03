490d3cfc5a816e178e4aa0ad25e72c2687e383af
Dan Stronge <dnlstronge@gmail.com>
2022-11-03 14:31:06 +0000

    add style add reset functionality for form

--- UdemyApp/udemyfirst/src/components/NewEventForm.js
+++ UdemyApp/udemyfirst/src/components/NewEventForm.js
@@ -9,6 +9,10 @@ export default function NewEventForm() {
 
     const [title, setTitle] = useState('')
     const [date, setDate] = useState('')
+    const resetForm = () => {
+        setTitle('')
+        setDate('')
+    }
 
    /* const handleChange = (e) => {
         setTitle(e.target.value)
@@ -18,17 +22,19 @@ export default function NewEventForm() {
 
 
   return (
-    <form clasName="new-event-form">
+    <form className="new-event-form">
         <label>
         <span>Event Title</span>
-            <input onChange={(e) => setTitle(e.target.value) } type="text" />
+            <input onChange={(e) => setTitle(e.target.value) } type="text" value={title} />
         </label>
         <label>
         <span>Event Date</span>
-            <input onChange={(e) => setDate(e.target.value)} type="date" />
+            <input onChange={(e) => setDate(e.target.value)} type="date" value={date}/>
         </label>
-        <button>Submit</button>
         <p>Title: {title} Date: {date}</p>
+        <p className="reset" onClick={resetForm}>Reset Form</p>
+        <button>Submit</button>
+        
     </form>
   )
 }
