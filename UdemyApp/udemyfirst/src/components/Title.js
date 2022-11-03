195a83efea1bdb4649b2c96d9655bb0ba293b5dd
Dan Stronge <dnlstronge@gmail.com>
2022-11-03 10:58:32 +0000

    creat compontent css for Title scoped titleblock so title/subtitle class may be reused

--- UdemyApp/udemyfirst/src/components/Title.js
+++ UdemyApp/udemyfirst/src/components/Title.js
@@ -1,11 +1,11 @@
-
+import './Title.css'
 
 export default function Title({titleprop, newprop}) {
     return (
-        <>
+        <div className="title-block">
             <h1 className="title">{titleprop} <br /> {newprop}</h1>
             <br />
             <h2 className="subtitle">Latest Events</h2>
-        </> 
+        </div> 
     )
 }
