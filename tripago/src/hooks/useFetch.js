125f6019f5024a0a5d75c0cbbbb7ccf34bf5f2be
Dan Stronge <dnlstronge@gmail.com>
2022-11-05 11:48:19 +0000

    update function to fetch data

--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -4,10 +4,16 @@ const useFetch = (url) => {
     const [data, setData] = useState(null)
     useEffect(() => {
         const fetchData = async () => {
-
+            const res = await fetch(url)
+            const json = await res.json()
+            setData(json)
+            fetchData() //invokes function to fetch data
         }
-    }, [])
+    }, [url])
 // not always possible but this time I can define the function
 //inside useeffect, avoiding the previous inf loop issue.
 }
+
+// a function within a function can be async - this is why this works
+// function needs to be invoked in order to fetch the data
 export default useFetch;
