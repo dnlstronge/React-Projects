21c315bbd6fc629fafadc9f639035dc88de34088
Dan Stronge <dnlstronge@gmail.com>
2022-11-05 11:08:47 +0000

    fix: useCallBack invoked to avoid inf loop caused by call to rerender use dependency array for 2nd arg of useCallBack

--- tripago/src/components/triplist.js
+++ tripago/src/components/triplist.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import './triplist.css'
 
 export default function Triplist() {
@@ -7,23 +7,32 @@ export default function Triplist() {
     const [url, setUrl] = useState('http://localhost:3000/trips')
 
     //I will create an async function here:
-    const fetchTrips = async () => {
+    // I wrap the entire function in usecallback
+    //this creates a cached version of a function
+    // it also needs a dep array as 2nd argument
+    const fetchTrips = useCallback(async () => {
       const response = await fetch(url)
       const json = await response.json()
         settrips(json)
       
-    }
+    }, [url])
     // now I need to invoke this function inside the useEffect hook:
-
+    // below is triggered to fetch new data due to dep array (url) in useCB
     useEffect(() => {
         fetchTrips()
-      }, [url])
+      }, [url, fetchTrips])
 
-//(clear!)
+
 
 // now what if one of the dependencies was a function?
 // what about async - useeffect cannot be async at present
 // as may cause issues in future - a function however?
+
+//NOTE - variable references are diff so react is re-render, 
+// this has cause inf loop
+// object or an array as a dep would cause the same issue
+//but could be wrapped in a state hook negating this
+// ENTER: USE CALLBACK
     console.log(trips)
 
         
