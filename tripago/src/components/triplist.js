18e1c50da32f6faad85ff2b3355203522c842809
Dan Stronge <dnlstronge@gmail.com>
2022-11-05 12:12:30 +0000

    Tidy: removed my notes, tested functionality works the same as prior usestate and useeffect but now with custom hook

--- tripago/src/components/triplist.js
+++ tripago/src/components/triplist.js
@@ -1,25 +1,19 @@
 import React, { useState } from 'react'
-import useFetch from '../hooks/useFetch'
+import { useFetch } from '../hooks/useFetch'
 import './triplist.css'
-
-//try creating a custom hook to bundle all of the fetch logic:
-// remove the state to for getting trips data - now got with custom hook
-// uses data from the new hook instead
 
 export default function Triplist() {
  
 
     const [url, setUrl] = useState('http://localhost:3000/trips')
-    const {data} = useFetch(url)
+    const {data: trips} = useFetch(url)
 
-   // removed logic as no longer needed
-
-  // need to changes 'trips' to data, as now cycling through 'data   
+  
   return (
     <div className="trip-list">
     <h2>Trip list</h2>
     <ul>
-        {data.map(trip => (
+        {trips && trips.map(trip => (
         <li key={trip.id}>
         <h3>{trip.title}</h3>
         <p>{trip.price}</p>
--- tripago/src/hooks/useFetch.js
+++ tripago/src/hooks/useFetch.js
@@ -1,28 +1,18 @@
 import {useState, useEffect} from 'react'
 
-const useFetch = (url) => {
+export const useFetch = (url) => {
     const [data, setData] = useState(null)
     useEffect(() => {
         const fetchData = async () => {
             const res = await fetch(url)
             const json = await res.json()
             setData(json)
-            fetchData() //invokes function to fetch data
+            
         }
+        fetchData() 
     }, [url])
 
-return {data}  // (data: data) can be shortened to (data) - it
-//does exactly the same thing
+return {data}  
 
-
-// not always possible but this time I can define the function
-//inside useeffect, avoiding the previous inf loop issue.
 }
 
-// a function within a function can be async - this is why this works
-// function needs to be invoked in order to fetch the data
-// last thing to do is return the data from the custom hook
-// e.g as an array or an object
-
-//note cannot cycle through a value of null! fix this.
-export default useFetch;
