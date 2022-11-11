34240ee7e4524176eae3bbc8f8d0ddb14f218b02
Dan Stronge <dnlstronge@gmail.com>
2022-11-11 11:08:01 +0000

    create: template for about&contact comp

---
+++ RouterProj/multipage-site/src/pages/Contact.js
@@ -0,0 +1,16 @@
+
+
+export default function Contact() {
+  return (
+    <div>
+      <h2>Contact
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Aliquid placeat veritatis commodi quam maiores eaque, 
+    voluptas autem doloremque est explicabo illum dolor 
+    laborum unde quasi in, architecto odit atque consectetur.
+        </p>
+      </h2>
+    </div>
+  )
+}
