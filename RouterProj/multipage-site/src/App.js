2df363141f58d9cdf10fe966dc41c1d67ea6bac0
Dan Stronge <dnlstronge@gmail.com>
2022-11-11 11:24:54 +0000

    Create: add routes in app.js

--- RouterProj/multipage-site/src/App.js
+++ RouterProj/multipage-site/src/App.js
@@ -1,11 +1,29 @@
 import './App.css'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+// Page comps
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>My Articles</h1>
-      </nav>
+    <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+        </nav>
+        <Route path="/">
+          <Home/>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
+    </BrowserRouter>
     </div>
   );
 }
