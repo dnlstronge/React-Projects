a883f6490fdfebdeaaa94c87529fe1cd2a11b6dc
Dan Stronge <dnlstronge@gmail.com>
2022-11-11 11:04:42 +0000

    Create: template for home component

---
+++ RouterProj/multipage-site/src/pages/Home.js
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div><h2>
+        Homepage
+    </h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Aliquid placeat veritatis commodi quam maiores eaque, 
+    voluptas autem doloremque est explicabo illum dolor 
+    laborum unde quasi in, architecto odit atque consectetur.
+    </p>
+    </div>
+  )
+}
