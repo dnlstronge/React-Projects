0d8dd60d84b1d35d3d5ec673c399891cbdb352c8
Dan Stronge <dnlstronge@gmail.com>
2022-11-12 09:50:54 +0000

    Create: new css file for Home import css to Home

--- RouterProj/multipage-site/src/pages/Home.js
+++ RouterProj/multipage-site/src/pages/Home.js
@@ -1,15 +1,23 @@
+import { useFetch } from "../hooks/useFetch"
 
+//styles
+import './Home.css'
 
 export default function Home() {
+    const {data: articles, isPending, error} = useFetch("http://localhost:3000/articles")
+
   return (
-    <div><h2>
-        Homepage
-    </h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-    Aliquid placeat veritatis commodi quam maiores eaque, 
-    voluptas autem doloremque est explicabo illum dolor 
-    laborum unde quasi in, architecto odit atque consectetur.
-    </p>
+    <div className="home">
+        <h2>Articles</h2>
+        {isPending && <div>Loading...</div>}
+        {error && <div>{error}</div>}
+        {articles && articles.map((article) => (
+            <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+            </div>
+            
+        ))}
     </div>
   )
 }
