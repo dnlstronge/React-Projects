d8debaa588111f59c5b4d9fbb0a2b5d57e17e6cd
Dan Stronge <dnlstronge@gmail.com>
2022-11-12 10:06:59 +0000

    Create: link for routes

--- RouterProj/multipage-site/src/pages/Home.js
+++ RouterProj/multipage-site/src/pages/Home.js
@@ -1,3 +1,5 @@
+
+import { Link } from "react-router-dom"
 import { useFetch } from "../hooks/useFetch"
 
 //styles
@@ -15,6 +17,9 @@ export default function Home() {
             <div key={article.id} className="card">
             <h3>{article.title}</h3>
             <p>{article.author}</p>
+            <Link to={`/articles/${article.id}`}>Read more...</Link>
+
+
             </div>
             
         ))}
