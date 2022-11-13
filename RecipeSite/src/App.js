106dd410144695fada8b4b2ccc5adf1a0d7d0a07
Dan Stronge <dnlstronge@gmail.com>
2022-11-13 10:40:34 +0000

    import(s) router-dom + components to app.js

--- RecipeSite/src/App.js
+++ RecipeSite/src/App.js
@@ -1,4 +1,15 @@
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css'
+
+//comps
+
+import Home from './pages/home/Home';
+import Create from './pages/create/Create';
+import Recipe from './pages/recipe/Recipe';
+import Search from './pages/search/Search';
+
+
 
 function App() {
   return (
