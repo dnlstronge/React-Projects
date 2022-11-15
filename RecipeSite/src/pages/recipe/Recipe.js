ed82bda8e66d053d0b2550209d3de786e0a5513f
Dan Stronge <dnlstronge@gmail.com>
2022-11-15 13:15:59 +0000

    // having issue with url returning undefined when should have url + id //this means an error will always throw on recipe.js as trying to fetch the data returns a 404

--- RecipeSite/src/pages/recipe/Recipe.js
+++ RecipeSite/src/pages/recipe/Recipe.js
@@ -1,10 +1,21 @@
-
+import { useParams } from 'react-router-dom'
+import { UseFetch } from '../../hooks/UseFetch.js'
 //styles
 import './Recipe.css'
 
 
-export default function recipe() {
+export default function Recipe() {
+  const { id } = useParams()
+  const url = "http://localhost:3000/recipes/" +id;
+  const {error, isPending, data: recipe} = UseFetch(url)
+
   return (
-    <div>recipe</div>
+    <div className="recipe">
+    {error && <p classsName="error">{error}</p>} 
+    {isPending && <p className="loading">Loading...</p>}
+    {recipe && <h1>{recipe.title}</h1>}
+    </div>
   )
 }
+// need to extract data (title) from json
+// pass as prop from home? use fetch?
