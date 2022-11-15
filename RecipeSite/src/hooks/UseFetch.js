3095d2a4327f24246530beb9336621d5a9e35334
Dan Stronge <dnlstronge@gmail.com>
2022-11-15 11:03:47 +0000

    fix: alter file names, resolve issue with hook import

--- RecipeSite/src/hooks/UseFetch.js
+++ RecipeSite/src/hooks/UseFetch.js
@@ -1,7 +1,6 @@
-
 import { useState, useEffect } from "react"
 
-export const useFetch = (url) => {
+export const UseFetch = (url) => {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
   const [error, setError] = useState(null)
--- RecipeSite/src/pages/home/Home.js
+++ RecipeSite/src/pages/home/Home.js
@@ -1,9 +1,16 @@
 
+import { UseFetch } from '../../hooks/UseFetch.js'
+
 //styles
 import './Home.css'
 
 export default function home() {
+  const {data, isPending, error} = UseFetch("http://localhost:3000/recipes")
+
   return (
-    <div>home</div>
+    <div className='home'>
+      {error && <p className='error'>{error}</p>}
+      
+    </div>
   )
 }
