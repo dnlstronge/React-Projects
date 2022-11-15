0e7a2475413df2484a30d3c121716d46d988859f
Dan Stronge <dnlstronge@gmail.com>
2022-11-15 13:29:36 +0000

    fix: typo recipes should be recipe caused id to return undefined caused link to break fixed

--- RecipeSite/src/components/RecipeList.js
+++ RecipeSite/src/components/RecipeList.js
@@ -11,7 +11,7 @@ export default function RecipeList({recipes}) {
                 <h3>{recipe.title}</h3>
                 <p>{recipe.cookingTime} to make.</p>
                 <div>{recipe.method.substring(0, 100)}...</div>
-                <Link to={`/recipes/${recipes.id}`}>Cook this</Link>
+                <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
             </div>
 
         ))}
