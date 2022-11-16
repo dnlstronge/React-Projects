ae7968d43b3a55049cd69dc9a1dc3aea4c7672a5
Dan Stronge <dnlstronge@gmail.com>
2022-11-16 13:37:41 +0000

    create: state for comp

--- RecipeSite/src/pages/create/Create.js
+++ RecipeSite/src/pages/create/Create.js
@@ -1,9 +1,23 @@
+import { useState } from 'react'
+
 // styles
 import './Create.css'
 
 
+// I will be creating a form
+// first  need 3 pieces of state: title, method, cooking time
+// will deal with ingredients later
 
-export default function create() {
+
+
+
+export default function Create() {
+
+const [title, setTitle] = useState('')
+const [method, setMethod] = useState('')
+const [cookingTime, setCookingTime] = useState('')
+
+
   return (
     <div>create</div>
   )
