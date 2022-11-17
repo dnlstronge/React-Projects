ebaef90b1b856dde078210af839088bce6e9c53c
Dan Stronge <dnlstronge@gmail.com>
2022-11-17 11:46:48 +0000

    create: add input + attributes + prop

--- RecipeSite/src/components/Searchbar.js
+++ RecipeSite/src/components/Searchbar.js
@@ -1,7 +1,23 @@
+import { useState } from 'react'
 import './Searchbar.css'
 
+// create state for search change value: 
+
+
 export default function Searchbar() {
+    const [term, setTerm] = useState('')
+
   return (
-    <div>Searchbar</div>
+    <div className='searchbar'>
+      <form onSubmit={}>
+        <label htmlFor="search">Search:</label>
+        <input 
+            type="text" 
+            id="search"
+            onChange={(e) => setTerm(e.target.value)}
+            required
+        />
+    </form>
+    </div>
   )
 }
