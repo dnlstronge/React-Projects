7a047640185ef91238c2bd3ef0f5d634c851e945
Dan Stronge <dnlstronge@gmail.com>
2022-11-17 11:58:03 +0000

    chore: tidy & clear notes

--- RecipeSite/src/components/Searchbar.js
+++ RecipeSite/src/components/Searchbar.js
@@ -5,7 +5,6 @@ import { useHistory } from 'react-router-dom'
 
 import './Searchbar.css'
 
-// create state for search change value: 
 
 
 export default function Searchbar() {
@@ -16,9 +15,6 @@ export default function Searchbar() {
         e.preventDefault()
         history.push(`/search?q${term}`)
     }
-// in handleSub I want to send the user to the search page....
-// query param :? ? goes on end of route for the search page
-
 
   return (
     <div className='searchbar'>
