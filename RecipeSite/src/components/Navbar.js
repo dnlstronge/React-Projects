fd5c1d070a8ef10fd2c5be12493630c1f77b2552
Dan Stronge <dnlstronge@gmail.com>
2022-11-17 12:34:21 +0000

    create: add template to function

--- RecipeSite/src/components/Navbar.js
+++ RecipeSite/src/components/Navbar.js
@@ -6,6 +6,10 @@ import { Link } from 'react-router-dom';
 //styles
 import './Navbar.css';
 
+// components:
+
+import Searchbar from './Searchbar';
+
 
 
 export default function Navbar() {
@@ -15,6 +19,7 @@ export default function Navbar() {
       <Link className='brand' to="/">
         <h1>Cooking Stuff!</h1>
       </Link>
+      <Searchbar />
       <Link to="/create">
         Create Recipe
       </Link>
--- RecipeSite/src/pages/search/Search.js
+++ RecipeSite/src/pages/search/Search.js
@@ -1,9 +1,33 @@
-//styles
+import { UseFetch } from '../../hooks/UseFetch'
+import { useLocation } from 'react-router-dom'
+
+//comps
+
+import RecipeList from '../../components/RecipeList'
+
+//style
 import './Search.css'
 
 
-export default function search() {
+export default function Search() {
+const queryString = useLocation()
+const queryParams = new URLSearchParams(queryString)
+const query = queryParams.get('q')
+
+//pass query string (?q:whatever) into useLocation
+// query gets the value of query parameter
+// send a request with useFetch to get whatever matches the param
+// need an endpoint (creat var to store it)
+
+const url = "http://localhost:3000/recipes?q=" + query
+const {error, isPending, data} = UseFetch(url)
+
   return (
-    <div>search</div>
+    <div>
+      <h2 className='page-title'>Recipes including "{query}"</h2>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
   )
 }
