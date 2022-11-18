a66d31e353b4df5602d0e4034e953289e808c8ad
Dan Stronge <dnlstronge@gmail.com>
2022-11-18 11:45:15 +0000

    create: add useReducer to ThemeContext

--- RecipeSite/src/context/ThemeContext.js
+++ RecipeSite/src/context/ThemeContext.js
@@ -1,9 +1,18 @@
-import { Context, createContext} from "react";
+import { createContext, useReducer} from "react";
 
 export const ThemeContext = createContext()
 
+const themeReducer = () => {}
+
 export function ThemeProvider( {children}) {
-    return <ThemeContext.Provider value={{color: "blue"}}> {children} </ThemeContext.Provider>
-}
+    useReducer(themeReducer, {
+        color: "blue"
+    })
+// second argument is the object w/ property to change
+    return (
+    <ThemeContext.Provider value={{color: "blue"}}> 
+    {children} 
+    </ThemeContext.Provider>
+)}
 
 
