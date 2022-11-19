b6623369ac789424c33dcc40a67361659e77c51a
Dan Stronge <dnlstronge@gmail.com>
2022-11-19 15:52:50 +0000

    create: add style and class to elements for DM toggle

--- RecipeSite/src/components/RecipeList.js
+++ RecipeSite/src/components/RecipeList.js
@@ -2,8 +2,12 @@ import { Link } from 'react-router-dom'
 //styles
 import './RecipeList.css'
 
+//hook:
+import { useTheme } from '../hooks/useTheme'
+
 
 export default function RecipeList({recipes}) {
+const { mode } = useTheme()
 
 if (recipes.length === 0) {
   return <div className='error'>No recipies found...</div>
@@ -12,7 +16,7 @@ if (recipes.length === 0) {
   return (
     <div className="recipe-list">
         {recipes.map(recipe => (
-            <div key={recipe.id} className="card">
+            <div key={recipe.id} className={`card ${mode}`}>
                 <h3>{recipe.title}</h3>
                 <p>{recipe.cookingTime} to make.</p>
                 <div>{recipe.method.substring(0, 100)}...</div>
