914742ab3d7ce130c32d87226516b1fec1658ab6
Dan Stronge <dnlstronge@gmail.com>
2022-11-20 12:12:52 +0000

    create:  add useEffect to fetch data from firebase

--- RecipeSite/src/pages/home/Home.js
+++ RecipeSite/src/pages/home/Home.js
@@ -1,15 +1,37 @@
 
-//hooks
-import { UseFetch } from '../../hooks/UseFetch.js'
+import { projectFirestore } from '../../firebase/config.js'
 
 //Components
 import RecipeList from '../../components/RecipeList.js'
 
+//hooks
+import { useEffect, useState } from 'react'
+
 //styles
 import './Home.css'
 
-export default function home() {
-  const {data, isPending, error} = UseFetch("http://localhost:3000/recipes")
+
+export default function Home() {
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(false)
+
+  useEffect( () => {
+    setIsPending(true)
+//returns a promise:
+    projectFirestore.collection('recipes').get().then((snapshot) => {
+      if(snapshot.empty) {
+        setError('No recipes to load...')
+        setIsPending(false)
+      } else {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          console.log(doc)
+        })
+      }
+    })
+
+  }, [])
 
   return (
     <div className='home'>
