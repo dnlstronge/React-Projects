8c412a2a22dd3df8affe63b48243757b401d4a90
Dan Stronge <dnlstronge@gmail.com>
2022-11-20 11:28:34 +0000

    create: add imports and config obj

---
+++ RecipeSite/src/firebase/config.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDmestd5vjR-4fnSmWc7xBMHD_yODdjwpQ",
+    authDomain: "cooking-stuff-dnls.firebaseapp.com",
+    projectId: "cooking-stuff-dnls",
+    storageBucket: "cooking-stuff-dnls.appspot.com",
+    messagingSenderId: "897180450694",
+    appId: "1:897180450694:web:bbb001b7a34150d8877607"
+  };
