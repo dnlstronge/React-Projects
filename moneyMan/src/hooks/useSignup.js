9a0939575ff6cc75a5e01b57bacb5f2092bf11cf
Dan Stronge <dnlstronge@gmail.com>
2022-11-23 12:40:16 +0000

    add state to function

--- moneyMan/src/hooks/useSignup.js
+++ moneyMan/src/hooks/useSignup.js
@@ -6,8 +6,19 @@ export const useSignup = () => {
     const [error, setError] = useState(null)
     const [isPending, setIsPending] = useState(false)
 
-    const signup = async (email, password, displayName => {
-        await
-    }
+    const signup = async (email, password, displayName) => {
+        setError(null)
+        setIsPending(true)
 
+      try {
+        // sign up user:
+        await projectAuth.createUserWithEmailAndPassword()
+      }  
+      catch(err) {
+        console.log(err.message)
+        setError(err.message)
+        setIsPending(false)
+      }
+    }
+return {error, isPending, signup}
 }
