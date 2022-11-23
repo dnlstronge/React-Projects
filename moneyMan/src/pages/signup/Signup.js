35f4910eb112c3691b983ba8aaaa0bfbac79f491
Dan Stronge <dnlstronge@gmail.com>
2022-11-23 11:34:56 +0000

    create: add state for signup Create: add form for sign up create: add handle submit function

--- moneyMan/src/pages/signup/Signup.js
+++ moneyMan/src/pages/signup/Signup.js
@@ -1,8 +1,46 @@
-import styles from './\Signup.module.css'
+import { useState } from 'react'
+
+
+// styles
+import styles from './Signup.module.css'
 
 
 export function Signup() {
+const [userName, setUserName] = useState('')
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+
+const handleSubmit = () => {
+
+}
+
   return (
-    <div>Signup</div>
+    <form className={styles['signup-form']}>
+      <label>
+      <span> Username:</span>
+        <input 
+          type="text"
+          onChange={(e) => setUserName(e.target.value)}
+          value={userName}
+        />
+      </label>
+      <label>
+      <span>Email:</span>
+      <input
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}    
+        value={email}  
+       />
+      </label>
+      <label>
+      <span>Password</span>
+      <input 
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      /> 
+      </label>
+      <button className='btn'>Signup</button>
+    </form>
   )
 }
