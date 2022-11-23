d5a9fc0be5df7da0cd6326f552df8fef48b68008
Dan Stronge <dnlstronge@gmail.com>
2022-11-23 15:20:01 +0000

    create: context folder and authcontext file

---
+++ moneyMan/src/context/AuthContext.js
@@ -0,0 +1,12 @@
+import { createContext, useReducer } from "react";
+
+export const AuthContext = createContext()
+
+export const AuthContextProvider = () => {
+
+    return (
+        <AuthContext.Provider>
+            
+        </AuthContext.Provider>
+    )
+}
