3db4544fafb1384aa4aec4cdefe67f3d23d7d685
Dan Stronge <dnlstronge@gmail.com>
2022-11-23 09:50:19 +0000

    create template form

--- moneyMan/src/pages/login/Login.js
+++ moneyMan/src/pages/login/Login.js
@@ -3,6 +3,19 @@ import styles from './Login.module.css'
 
 export function Login() {
   return (
-    <div>Login</div>
+  <form className={styles['login-form']}>
+    <h2>Login</h2>
+    <label>
+    <span>Email:</span>
+      <input type="email"
+      
+      />
+    </label>
+    <label>
+      <span>Password:</span>
+      <input type="password" />
+    </label>
+    <button className="btn">Login</button>
+  </form>
   )
 }
