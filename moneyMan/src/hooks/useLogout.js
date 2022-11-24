4d64795dbe8ed95a413cb01eed2852db6912f5d3
Dan Stronge <dnlstronge@gmail.com>
2022-11-24 13:53:38 +0000

    create: state + destruct dispatch function

--- moneyMan/src/hooks/useLogout.js
+++ moneyMan/src/hooks/useLogout.js
@@ -4,5 +4,8 @@ import { useAuthContext } from "./useAuthContext"
 
 
 const useLogout = () => {
-
+    const [error, setError] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const { dispatch } = useAuthContext()
+    
 }
