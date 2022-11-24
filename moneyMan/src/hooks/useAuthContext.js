084606383cf5265e3d0d894229f91404bd1a822a
Dan Stronge <dnlstronge@gmail.com>
2022-11-24 09:34:02 +0000

    fix: import error in index.ja

--- moneyMan/src/hooks/useAuthContext.js
+++ moneyMan/src/hooks/useAuthContext.js
@@ -5,4 +5,10 @@ import { useContext } from "react";
 
 export const useAuthContext = () => {
     const context = useContext(AuthContext) // object containing ...state + dispatch
+
+    if(!context) {
+        throw Error('useAuthContext must be inside AuthContext provider')
+    }
+
+    return context
 }
