7a71120c123f9af10abf555e1a0a02589f481931
Dan Stronge <dnlstronge@gmail.com>
2022-11-25 09:56:13 +0000

    create: add logout btn and function to navbar

--- moneyMan/src/components/Navbar.js
+++ moneyMan/src/components/Navbar.js
@@ -2,6 +2,7 @@
 //imports
 
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 //style
 
@@ -9,12 +10,21 @@ import styles from './Navbar.module.css'
 
 
 export function Navbar() {
+
+  const { logout } = useLogout()
+
+
+
+
   return (
     <nav className={styles.navbar}>
     <ul>
         <li className={styles.title}>moneyMan</li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Signup</Link></li>
+        <li>
+          <button className="btn" onClick={ logout }>Logout</button>
+        </li>
     </ul>
 
     </nav>
