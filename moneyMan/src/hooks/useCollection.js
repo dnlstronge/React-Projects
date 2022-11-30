5e836095fdafda6ab83246ee15260a087cd800b7
Dan Stronge <dnlstronge@gmail.com>
2022-11-30 13:37:54 +0000

    complete usecollection hook

--- moneyMan/src/hooks/useCollection.js
+++ moneyMan/src/hooks/useCollection.js
@@ -6,4 +6,31 @@ export const useCollection = (collection) => {
     const [ documents, setDocuments ] = useState(null)
     const [ error, setError ] = useState(null)
 
+
+    // below: when using snap shot two args, 1st sets state for docs and error
+    // the second handles error
+
+    useEffect(() => {
+        let ref = projectFirestore.collection(collection)
+        const unsubscribe = ref.onSnapshot( (snapshot) => {
+            let results = []
+            snapshot.docs.forEach( doc => {
+                results.push({...doc.data, id: doc.id })
+            })
+
+            // updates state
+            setDocuments(results)
+            setError(null)
+        }, (error) => {
+            console.log(error)
+            setError('Could not fetch data')
+        })
+            // cleanup/unsub
+        
+            return () => unsubscribe()
+
+
+    }, [collection])
+
+    return {documents, error}
 }
