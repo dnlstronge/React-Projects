32bfb3c471d902cf6500e86aa63df5ddf7f47da1
Dan Stronge <dnlstronge@gmail.com>
2022-12-01 10:00:58 +0000

    import usecollection to home start output of template need to create transactionList comp

--- moneyMan/src/pages/home/Home.js
+++ moneyMan/src/pages/home/Home.js
@@ -5,6 +5,7 @@ import styles from './Home.module.css'
 // hooks 
 
 import { useAuthContext } from '../../hooks/useAuthContext'
+import { useCollection } from '../../hooks/useCollection'
 
 //comps
 
@@ -12,11 +13,14 @@ import TransactionForm  from './TransactionForm'
 
 export function Home() {
   const { user } = useAuthContext()
+  const { documents, error } = useCollection('transactions')
 
   return (
     <div className={styles.container}>
 
-    <div className={styles.content}>transaction list
+    <div className={styles.content}>
+    {error && <p>{error}</p>}
+    {documents && <TransactionList />}
     
       </div>
 
