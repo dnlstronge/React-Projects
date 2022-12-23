6a7ec200456c31f9e9788ebf676eb41c2fccd187
Dan Stronge <dnlstronge@gmail.com>
2022-12-23 10:41:37 +0000

    update: changes to routes as of RR6

--- RecipeSite/src/App.js
+++ RecipeSite/src/App.js
@@ -23,24 +23,16 @@ function App() {
   const { mode } = useTheme()
 
   return (
-    <div className={`App ${modeRoutes}`}>
+    <div className={`App ${mode}`}>
       <BrowserRouter>
         <Navbar />
         <ThemeSelector /> 
         <Routes>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/search">
-              <Search />
-            </Route>
-            <Route path="/recipes/:id"> 
-              <Recipe />
-            </Route>
-          </Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/create" element={<Create />}/>
+            <Route path="/search" element={<Search />}/>
+            <Route path="/recipes/:id" element={<Recipe />}/> 
+        </Routes>
       </BrowserRouter>
     </div>
   );
