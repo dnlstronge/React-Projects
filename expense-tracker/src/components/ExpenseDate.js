19920c68ff0cee88055da2a6045c28116327b62b
Dan Stronge <dnlstronge@gmail.com>
2023-01-11 16:33:14 +0000

    [x] - new css file for new components [x] -  implement new css in ExpenseDate

--- expense-tracker/src/components/ExpenseDate.js
+++ expense-tracker/src/components/ExpenseDate.js
@@ -1,4 +1,5 @@
-
+//styles
+import './ExpenseDate.css'
 
 function ExpenseDate(props) {
 
@@ -7,10 +8,10 @@ function ExpenseDate(props) {
     const year = props.date.getFullYear()
 
     return (
-     <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
+     <div className='expense-date'>
+        <div className='expense-date__day'>{day}</div>
+        <div className='expense-date__month'>{month}</div>
+        <div className='expense-date__year'>{year}</div>
     </div>
     )
     
