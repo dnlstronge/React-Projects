4fbe423712bb115738d35894b55e97e6288b1581
Dan Stronge <dnlstronge@gmail.com>
2023-01-11 17:14:39 +0000

    [x] - moved array back to App [x] - rename&refactor props naming overuse [x] - expenses now display as before but less clutter

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -5,12 +5,32 @@
 import Expenses from './components/Expenses'
 
 
-function App() {
+function App(props) {
+
+  const expenses = [
+    {
+     id: 'e1', 
+     title: 'Cinema' ,
+     amount: 25.55, 
+     date: new Date(2023, 0, 11)},
+
+     {
+     id: 'e2',
+     title: 'Eating Out' ,
+     amount: 74.45 , 
+     date: new Date(2023, 0, 11)},
+
+     {
+     id: 'e3',
+     title: 'Netflix' ,
+     amount: 9.99 , 
+     date: new Date(2023, 0, 11)}
+  ]
  
   return (
     <div className="App">
       <h2>Heyho.... let's go!</h2>
-      <Expenses />
+      <Expenses items={expenses}/>
       
    
     </div>
--- expense-tracker/src/components/Expenses.js
+++ expense-tracker/src/components/Expenses.js
@@ -1,43 +1,26 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
+import App from '../App'
 
-function Expenses () {
+function Expenses (props) {
 
-    const expenses = [
-        {
-         id: 'e1', 
-         title: 'Cinema' ,
-         amount: 25.55, 
-         date: new Date(2023, 0, 11)},
     
-         {
-         id: 'e2',
-         title: 'Eating Out' ,
-         amount: 74.45 , 
-         date: new Date(2023, 0, 11)},
-    
-         {
-         id: 'e3',
-         title: 'Netflix' ,
-         amount: 9.99 , 
-         date: new Date(2023, 0, 11)}
-      ]
     
     return (
        <div className='expenses'>
          <ExpenseItem 
-        title={expenses[0].title} 
-        amount={expenses[0].amount} 
-        date={expenses[0].date}/>
+        title={props.items[0].title} 
+        amount={props.items[0].amount} 
+        date={props.items[0].date}/>
       <ExpenseItem 
-        title={expenses[1].title} 
-        amount={expenses[1].amount} 
-        date={expenses[1].date}
+        title={props.items[1].title} 
+        amount={props.items[1].amount} 
+        date={props.items[1].date}
       />
       <ExpenseItem 
-        title={expenses[2].title} 
-        amount={expenses[2].amount} 
-        date={expenses[2].date}
+        title={props.items[2].title} 
+        amount={props.items[2].amount} 
+        date={props.items[2].date}
       />
        </div>
     )
