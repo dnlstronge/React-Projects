46a04c9679316edba2390c3d18d91d73a6580e6f
Dan Stronge <dnlstronge@gmail.com>
2023-01-11 16:19:29 +0000

    [x] - Split component, date format is now calculated separately

---
+++ expense-tracker/src/components/ExpenseDate.js
@@ -0,0 +1,20 @@
+
+
+function ExpenseDate(props) {
+
+    const day = props.date.toLocaleString("en-US", { day: "2-digit"})
+    const month = props.date.toLocaleString("en-US", { month: "long" })
+    const year = props.date.getFullYear()
+
+    return (
+     <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+    </div>
+    )
+    
+
+}
+
+export default ExpenseDate;
