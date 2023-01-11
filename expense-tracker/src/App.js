77ed44f0a6f5d0993ee6819cbadb0e33241dd4d8
Dan Stronge <dnlstronge@gmail.com>
2023-01-11 15:09:09 +0000

    [] - items now output via props

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -30,7 +30,21 @@ function App() {
     <div className="App">
       <h2>Heyho.... let's go!</h2>
       
-      <ExpenseItem />
+      <ExpenseItem 
+        title={expenses[0].title} 
+        amount={expenses[0].amount} 
+        date={expenses[0].date}/>
+      <ExpenseItem 
+        title={expenses[1].title} 
+        amount={expenses[1].amount} 
+        date={expenses[1].date}
+      />
+      <ExpenseItem 
+        title={expenses[2].title} 
+        amount={expenses[2].amount} 
+        date={expenses[2].date}
+      />
+   
     </div>
   );
 }
--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -3,18 +3,15 @@ import './ExpenseItem.css'
 
 
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-    const expenseDate = new Date(2023, 0 , 11)
-    const expenseTitle = "Eating out"
-    const expenseAmount = 75.42
-
+   
 return (
     <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
+        <div>{props.date.toISOString()}</div>
         <div className="expense-item__description">
-            <h2>{expenseTitle}</h2>
-            <div className="expense-item__price">£{expenseAmount}</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">£{props.amount}</div>
         </div>
     </div> 
     
