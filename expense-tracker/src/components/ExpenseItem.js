8bcc26284bcfc18aa0706db160532a4f51257f9f
Dan Stronge <dnlstronge@gmail.com>
2023-01-11 13:41:08 +0000

    [] - add css

--- expense-tracker/src/components/ExpenseItem.js
+++ expense-tracker/src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
+// styles
+import './ExpenseItem.css'
+
 
 function ExpenseItem() {
 return (
     <div>
-        <div>Date:</div>
+        <div>11th Jan 2023</div>
         <div>
-            <h2>Title</h2>
-            <div>Amount</div>
+            <h2>Eating out</h2>
+            <div>£75.40</div>
         </div>
     </div> 
     
