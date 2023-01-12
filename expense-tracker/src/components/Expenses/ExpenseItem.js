75d9423f2b8e7790eeb34089ceb14944b362a5db
Dan Stronge <dnlstronge@gmail.com>
2023-01-12 11:33:33 +0000

    [x] - fix syntax error

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -7,18 +7,20 @@ import Card from '../UI/Card';
 
 
 const ExpenseItem = (props) => {
+    let title = props.title
+
     const handleClick = () => {
-        
+        title = 'updated'
     }
     
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">£{props.amount}</div>
             </div>
-            <button onCLick={handleClick}>Change Title</button>
+            <button onClick={handleClick}>Change Title</button>
         </Card> 
         
         
