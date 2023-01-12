a90e1bea2c00e7e34cff013d0f69e3cdbdb3c8d7
Dan Stronge <dnlstronge@gmail.com>
2023-01-12 16:12:28 +0000

    [x] - complete template for ExpenseForm

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -2,9 +2,28 @@
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-    <div>
-        <form></form>
-    </div>
+   
+      return (  
+        <form>
+            <div className='new-expense__controls'>
+               <div className="new-expense__control">
+                    <label>Title:</label>
+                    <input type="text" />
+               </div>
+               <div className="new-expense__control">
+                    <label>Amount:</label>
+                    <input type="number" min="0.01" step="0.01"/>
+               </div>
+               <div className="new-expense__control">
+                    <label>Date:</label>
+                    <input type="date" min="2021-01-01" max="2024-01-01"/>
+               </div>
+            </div>
+            <div className="new-expense__actions">
+                <button type="submit" className="btn">Add expense</button>
+            </div>
+        </form> )
+   
 }
 
 export default ExpenseForm
