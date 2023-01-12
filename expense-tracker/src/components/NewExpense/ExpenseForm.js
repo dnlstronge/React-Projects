1d74eae941182f4267531bf5a17bb13ab2042727
Dan Stronge <dnlstronge@gmail.com>
2023-01-12 16:41:37 +0000

    [x] - set up listeners for form fields [x] - add event handlers for fields [x] - add state for handlers

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,40 @@
 //styles
 import './ExpenseForm.css'
 
+//imports:
+import { useState } from 'react'
+
 const ExpenseForm = () => {
-   
+
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+
+  const titleChangeHandler = (e) => { 
+     setEnteredTitle(e.target.value)
+    
+   }
+  const amountChangeHandler = (e) => {
+     setEnteredAmount(e.target.value)
+  }
+  const dateChangeHandler = (e) => {
+     setEnteredDate(e.target.value)
+  }
+
       return (  
         <form>
             <div className='new-expense__controls'>
                <div className="new-expense__control">
                     <label>Title:</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleChangeHandler}/>
                </div>
                <div className="new-expense__control">
                     <label>Amount:</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
                </div>
                <div className="new-expense__control">
                     <label>Date:</label>
-                    <input type="date" min="2021-01-01" max="2024-01-01"/>
+                    <input type="date" min="2021-01-01" max="2024-01-01" onChange={dateChangeHandler}/>
                </div>
             </div>
             <div className="new-expense__actions">
