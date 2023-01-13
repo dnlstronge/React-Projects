bd8336e09eb14c7f3acb46dd1647557dd911bf94
Dan Stronge <dnlstronge@gmail.com>
2023-01-13 14:42:20 +0000

    Chore: tidy

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -17,26 +17,26 @@ const Expenses = (props) => {
       <div>
       
        <Card className='expenses'>
-       <ExpensesFilter value={dateSelect} onDateSelect={onDateSelectHandler}/>
+         <ExpensesFilter value={dateSelect} onDateSelect={onDateSelectHandler}/>
          <ExpenseItem 
-        title={props.items[0].title} 
-        amount={props.items[0].amount} 
-        date={props.items[0].date}/>
-      <ExpenseItem 
-        title={props.items[1].title} 
-        amount={props.items[1].amount} 
-        date={props.items[1].date}
-      />
-      <ExpenseItem 
-        title={props.items[2].title} 
-        amount={props.items[2].amount} 
-        date={props.items[2].date}
-      />
-      <ExpenseItem 
-        title={props.items[3].title} 
-        amount={props.items[3].amount} 
-        date={props.items[3].date}
-      />
+            title={props.items[0].title} 
+            amount={props.items[0].amount} 
+            date={props.items[0].date}/>
+        <ExpenseItem 
+            title={props.items[1].title} 
+            amount={props.items[1].amount} 
+            date={props.items[1].date}
+        />
+        <ExpenseItem 
+            title={props.items[2].title} 
+            amount={props.items[2].amount} 
+            date={props.items[2].date}
+        />
+        <ExpenseItem 
+            title={props.items[3].title} 
+            amount={props.items[3].amount} 
+            date={props.items[3].date}
+        />
        </Card>
        </div>
     )
