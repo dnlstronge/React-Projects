8b96dcdb7d2c7fd3a0afff59c2d419b0df571cb3
Dan Stronge <dnlstronge@gmail.com>
2023-01-13 10:53:14 +0000

    [x] - refactor x 3 state in to one piece of state

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -6,19 +6,24 @@ import { useState } from 'react'
 
 const ExpenseForm = () => {
 
-  const [enteredTitle, setEnteredTitle] = useState('')
-  const [enteredAmount, setEnteredAmount] = useState('')
-  const [enteredDate, setEnteredDate] = useState('')
+//   const [enteredTitle, setEnteredTitle] = useState('')
+//   const [enteredAmount, setEnteredAmount] = useState('')
+//   const [enteredDate, setEnteredDate] = useState('')
+
+   const [userInput, setUserInput] = useState({
+                                        enteredTitle: '', 
+                                        enteredAmount: '', 
+                                        enteredDate: ''})   
 
   const titleChangeHandler = (e) => { 
-     setEnteredTitle(e.target.value)
+     setUserInput({...userInput, enteredTitle: e.target.value})
     
    }
   const amountChangeHandler = (e) => {
-     setEnteredAmount(e.target.value)
+     setUserInput({...userInput, enteredAmount: e.target.value})
   }
   const dateChangeHandler = (e) => {
-     setEnteredDate(e.target.value)
+    setUserInput({...userInput, enteredDate: e.target.value})
   }
 
       return (  
