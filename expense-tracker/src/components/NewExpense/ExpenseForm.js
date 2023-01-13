db4c312be0548ac32a885510f117b91d3ce5f264
Dan Stronge <dnlstronge@gmail.com>
2023-01-13 11:30:19 +0000

    [x] - test in browser that expenseData object is created (logged to console) -- (clear)

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -40,8 +40,9 @@ const ExpenseForm = () => {
      const expenseData = {
           title: enteredTitle,
           amount: enteredAmount,
-          date: enteredDate
+          date: new Date(enteredDate)
      }
+     console.log(expenseData)
   }
 
       return (  
