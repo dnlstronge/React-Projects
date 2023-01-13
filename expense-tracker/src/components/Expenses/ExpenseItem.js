a6cd5c5fa8b9e00cb54331a52d8748000b4dda79
Dan Stronge <dnlstronge@gmail.com>
2023-01-13 15:08:46 +0000

    [x] - removed test button & state

--- expense-tracker/src/components/Expenses/ExpenseItem.js
+++ expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -4,26 +4,19 @@ import './ExpenseItem.css';
 //imports
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-import { useState } from 'react'
+
 
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title)
-    
-    const handleClick = () => {
-        setTitle('update')
-        console.log('updated!');
-        
-    }
+   
     
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">£{props.amount}</div>
             </div>
-            <button onClick={handleClick}>Change Title</button>
         </Card> 
         
         
