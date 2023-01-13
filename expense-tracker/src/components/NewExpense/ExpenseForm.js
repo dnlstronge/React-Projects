e9aba3ccafa03c8536723e51ce8a72fb1383905e
Dan Stronge <dnlstronge@gmail.com>
2023-01-13 11:51:17 +0000

    [x] - Added double binding [x] - form now clears on submit

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -43,6 +43,9 @@ const ExpenseForm = () => {
           date: new Date(enteredDate)
      }
      console.log(expenseData)
+     setEnteredTitle('')
+     setEnteredAmount('')
+     setEnteredDate('')
   }
 
       return (  
@@ -50,15 +53,28 @@ const ExpenseForm = () => {
             <div className='new-expense__controls'>
                <div className="new-expense__control">
                     <label>Title:</label>
-                    <input type="text" onChange={titleChangeHandler}/>
+                    <input 
+                         type="text" 
+                         value={enteredTitle} 
+                         onChange={titleChangeHandler}/>
                </div>
                <div className="new-expense__control">
                     <label>Amount:</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                    <input 
+                         type="number" 
+                         min="0.01" 
+                         step="0.01" 
+                         value={enteredAmount} 
+                         onChange={amountChangeHandler}/>
                </div>
                <div className="new-expense__control">
                     <label>Date:</label>
-                    <input type="date" min="2021-01-01" max="2024-01-01" onChange={dateChangeHandler}/>
+                    <input 
+                         type="date" 
+                         min="2021-01-01" 
+                         max="2024-01-01" 
+                         value={enteredDate}
+                         onChange={dateChangeHandler}/>
                </div>
             </div>
             <div className="new-expense__actions">
