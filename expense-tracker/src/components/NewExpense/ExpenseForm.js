c336cbebe76a9b84d8929f7e0dd56eea2c17f2f9
Dan Stronge <dnlstronge@gmail.com>
2023-01-15 12:16:18 +0000

    [x] - submit button also fires toggle returning state back to false (hides form) [x] - test button function in browser (clear)

--- expense-tracker/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/components/NewExpense/ExpenseForm.js
@@ -46,7 +46,7 @@ const ExpenseForm = (props) => {
      setEnteredTitle('')
      setEnteredAmount('')
      setEnteredDate('')
-    
+    props.toggleForm(false)
      
   }
   const onCancelHandler = () => {
@@ -85,7 +85,7 @@ const ExpenseForm = (props) => {
                </div>
             </div>
             <div className="new-expense__actions">
-                <button onClick={onCancelHandler}>Cancel</button>
+                <button type="button" onClick={onCancelHandler}>Cancel</button>
                 <button type="submit" >Add expense</button>
             </div>
         </form> 
